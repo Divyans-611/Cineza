0c024cc949e025a3693fff419610739b47082646
Divyans Srivastava <divysrivastav5@gmail.com>
2026-06-26 23:29:45 +0530

    Finishhed phase 5

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,36 @@
+import dns from 'dns'
+
+// Override DNS lookup for TMDB domains to bypass ISP DNS hijacking/blocking in India.
+// Queries public Google and Cloudflare DNS directly instead of OS resolution.
+const originalLookup = dns.lookup
+const resolver = new dns.Resolver()
+resolver.setServers(['8.8.8.8', '1.1.1.1'])
+
+dns.lookup = (hostname, options, callback) => {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+  
+  if (hostname && (hostname.includes('themoviedb.org') || hostname.includes('tmdb.org'))) {
+    resolver.resolve4(hostname, (err, addresses) => {
+      if (err || !addresses || addresses.length === 0) {
+        return originalLookup(hostname, options, callback)
+      }
+      if (options && options.all) {
+        const mapped = addresses.map(addr => ({ address: addr, family: 4 }))
+        callback(null, mapped)
+      } else {
+        callback(null, addresses[0], 4)
+      }
+    })
+  } else {
+    originalLookup(hostname, options, callback)
+  }
+}
+
+dns.setDefaultResultOrder('ipv4first')
+
 import dotenv from 'dotenv'
 import app from './app.js'
 import config from './config/index.js'
